180ee2941be84c492f1a7ea01e1eef2d9a736d39
Dan Leehr <dleehr@redhat.com>
2026-01-05 15:00:00 -0500

    set -x on start-dev.sh

--- start-dev.sh
+++ start-dev.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -x
+
 mkdir -p config
 
 vault server -dev -dev-root-token-id root -dev-tls -dev-tls-san=host.docker.internal -dev-cluster-json="config/cluster.json" -dev-listen-address=0.0.0.0:8200
